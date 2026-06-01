847acf6a8f04bf5869005679f46c5962940e365e
loukika sawant <loukikasawant4@gmail.com>
2026-06-01 22:21:49 +0530

    Implement add task feature

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Todo App Started");
+const addBtn = document.getElementById("addBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", () => {
+
+    const taskText = taskInput.value;
+
+    if(taskText === "")
+        return;
+
+    const li = document.createElement("li");
+
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+});
